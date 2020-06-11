cd5540d982a215541ba2d734c9da0a50b317557c
Dinho Zanardi <prof.dinho.ti@gmail.com>
2020-06-11 18:34:11 -0300

    Inclusão do MySQL

--- src/public/index.php
+++ src/public/index.php
@@ -1,4 +1,5 @@
 <?php
+/*
 $db_host  = 'postgres';
 $db_name  = 'profdinho';
 $db_login = 'postgres';
@@ -20,4 +21,36 @@ if(!($result = pg_query($conexao, $sql))) {
 while ($row = pg_fetch_array($result)) {
     echo $row['id'] . "; " . $row['nome'] . "; " . $row['celular'] . "; " . $row['email'] . "<br>";
 }
+*/
+header("Content-type: text/html; charset=utf-8");
+//Nome do servidor que está o banco de dados
+//$servidor = "cincoacertos.mysql.dbaas.com.br";
+$servidor = "mysql";
+//Usuário do banco de dados
+$usuario = "aluno";
+//Senha do banco de dados
+$senha = "123456";
+//Nome da base de dados
+$dbname = "profdinho";
+
+//Criar a conexao com MySQLi
+$conexao = mysqli_connect($servidor, $usuario, $senha, $dbname);
+// Verifica conexão
+/*
+if (!$conexao) {
+    die("Falha na conexão: " . mysqli_connect_error());
+}
+*/
+if(mysqli_connect_error()) { // < evite IF sem chaves
+    die("Falha na conexão: " . mysqli_connect_error());
+} else {
+    mysqli_query($conexao,"SET NAMES 'utf8'");
+    mysqli_query($conexao,'SET character_set_connection=utf8');
+    mysqli_query($conexao,'SET character_set_client=utf8');
+    mysqli_query($conexao,'SET character_set_results=utf8');
+}
+
+mysqli_set_charset($conexao,"utf8");
+date_default_timezone_set('America/Sao_Paulo');
+echo "Conectou";
 ?>
